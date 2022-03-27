a86d5d7b3e3309957f3b4c74908f0dcafb8810b4
onirutlA <alturino001@gmail.com>
2022-03-27 07:44:11 +0700

    add: PageResponse for holding MovieResponse and TvResponse

---
+++ app/src/main/java/com/onirutla/movflex/data/PageResponse.kt
@@ -0,0 +1,15 @@
+package com.onirutla.movflex.data
+
+
+import com.squareup.moshi.Json
+
+data class PageResponse<T>(
+    @Json(name = "page")
+    val page: Int = 0,
+    @Json(name = "results")
+    val responses: List<T> = listOf(),
+    @Json(name = "total_pages")
+    val totalPages: Int = 0,
+    @Json(name = "total_results")
+    val totalResults: Int = 0
+)
