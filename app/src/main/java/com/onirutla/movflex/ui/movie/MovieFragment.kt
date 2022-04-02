9de11b6da06fbfa2e73f1f0172251022a33d5b50
onirutlA <alturino001@gmail.com>
2022-04-02 11:39:54 +0700

    refactor: move MovieFragment.kt to ui package

---
+++ app/src/main/java/com/onirutla/movflex/ui/movie/MovieFragment.kt
@@ -0,0 +1,62 @@
+package com.onirutla.movflex.ui.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.onirutla.movflex.databinding.FragmentMovieBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MovieFragment : Fragment() {
+
+    private val viewModel: MovieViewModel by viewModels()
+
+    private val categoryAdapter: MovieCategoryAdapter by lazy {
+        MovieCategoryAdapter(itemClickListener = { view, movie -> }) { category ->
+
+        }
+    }
+
+    private var _binding: FragmentMovieBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeData()
+
+        binding.movieHomeList.apply {
+            adapter = categoryAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movie.collect {
+                    categoryAdapter.submitList(it.toMutableList())
+                }
+            }
+        }
+    }
+
+}
