28380ca482faea32324d75f8a7c083d3a9aab473
onirutlA <alturino001@gmail.com>
2022-04-02 11:32:47 +0700

    refactor: remove unnecessary variable in TvResponse.kt

--- app/src/main/java/com/onirutla/movflex/data/source/remote/response/tv/TvResponse.kt
+++ app/src/main/java/com/onirutla/movflex/data/source/remote/response/tv/TvResponse.kt
@@ -1,37 +1,18 @@
 package com.onirutla.movflex.data.source.remote.response.tv
 
-import com.onirutla.movflex.data.source.local.entities.TvEntity
-import com.squareup.moshi.Json
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class TvResponse(
-    @Json(name = "backdrop_path")
-    val backdropPath: String = "",
-    @Json(name = "first_air_date")
-    val firstAirDate: String = "",
-    @Json(name = "genre_ids")
-    val genreIds: List<Int> = listOf(),
     @Json(name = "id")
     val id: Int = 0,
-    @Json(name = "name")
-    val name: String = "",
-    @Json(name = "origin_country")
-    val originCountry: List<String> = listOf(),
-    @Json(name = "original_language")
-    val originalLanguage: String = "",
-    @Json(name = "original_name")
-    val originalName: String = "",
-    @Json(name = "overview")
-    val overview: String = "",
-    @Json(name = "popularity")
-    val popularity: Double = 0.0,
     @Json(name = "poster_path")
     val posterPath: String = "",
-    @Json(name = "vote_average")
-    val voteAverage: Double = 0.0,
-    @Json(name = "vote_count")
-    val voteCount: Int = 0
 )
 
+/*
 fun TvResponse.toEntity() = TvEntity(
     backdropPath,
     firstAirDate,
@@ -46,4 +27,4 @@ fun TvResponse.toEntity() = TvEntity(
     posterPath,
     voteAverage,
     voteCount
-)
+)*/
