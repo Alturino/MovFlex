034d0039dce71b560e92fcb3c63ae9b1f6df938c
onirutlA <alturino001@gmail.com>
2022-04-02 11:40:08 +0700

    refactor: move TvFragment.kt to ui package

---
+++ app/src/main/java/com/onirutla/movflex/ui/tv/TvFragment.kt
@@ -0,0 +1,62 @@
+package com.onirutla.movflex.ui.tv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.onirutla.movflex.databinding.FragmentTvBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class TvFragment : Fragment() {
+
+    private var _binding: FragmentTvBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: TvViewModel by viewModels()
+
+    private val categoryAdapter: TvCategoryAdapter by lazy {
+        TvCategoryAdapter(itemClickListener = { view, movie -> }) { category ->
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTvBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tvHome.collect {
+                    categoryAdapter.submitList(it)
+                }
+            }
+        }
+
+        binding.tvHomeList.apply {
+            adapter = categoryAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
