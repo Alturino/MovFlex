47ffa6c4d8b5c618fef9f2077cae18fb9020e340
onirutlA <alturino001@gmail.com>
2022-04-02 11:32:42 +0700

    refactor: remove unnecessary variable in MovieResponse.kt

--- app/src/main/java/com/onirutla/movflex/data/source/remote/response/movie/MovieResponse.kt
+++ app/src/main/java/com/onirutla/movflex/data/source/remote/response/movie/MovieResponse.kt
@@ -1,52 +1,12 @@
 package com.onirutla.movflex.data.source.remote.response.movie
 
-import com.onirutla.movflex.data.source.local.entities.MovieEntity
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class MovieResponse(
-    @Json(name = "adult")
-    val adult: Boolean = false,
-    @Json(name = "backdrop_path")
-    val backdropPath: String = "",
-    @Json(name = "genre_ids")
-    val genreIds: List<Int> = listOf(),
     @Json(name = "id")
     val id: Int = 0,
-    @Json(name = "original_language")
-    val originalLanguage: String = "",
-    @Json(name = "original_title")
-    val originalTitle: String = "",
-    @Json(name = "overview")
-    val overview: String = "",
-    @Json(name = "popularity")
-    val popularity: Double = 0.0,
     @Json(name = "poster_path")
     val posterPath: String = "",
-    @Json(name = "release_date")
-    val releaseDate: String = "",
-    @Json(name = "title")
-    val title: String = "",
-    @Json(name = "video")
-    val video: Boolean = false,
-    @Json(name = "vote_average")
-    val voteAverage: Double = 0.0,
-    @Json(name = "vote_count")
-    val voteCount: Int = 0
-)
-
-fun MovieResponse.toEntity() = MovieEntity(
-    adult,
-    backdropPath,
-    genreIds,
-    id,
-    originalLanguage,
-    originalTitle,
-    overview,
-    popularity,
-    posterPath,
-    releaseDate,
-    title,
-    video,
-    voteAverage,
-    voteCount
 )
