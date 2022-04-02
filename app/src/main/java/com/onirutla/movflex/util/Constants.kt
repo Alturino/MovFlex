363540c67647c468e85f310ada4424ccca5fe381
onirutlA <alturino001@gmail.com>
2022-04-02 11:17:02 +0700

    add: Constants.kt

---
+++ app/src/main/java/com/onirutla/movflex/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.onirutla.movflex.util
+
+import com.onirutla.movflex.BuildConfig
+
+object Constants {
+    const val TMDB_STARTING_PAGE_INDEX = 1
+    const val NETWORK_LOAD_SIZE = 20
+    const val API_TOKEN = BuildConfig.API_TOKEN
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val BASE_IMAGE_PATH = BuildConfig.BASE_IMAGE_PATH
+}
