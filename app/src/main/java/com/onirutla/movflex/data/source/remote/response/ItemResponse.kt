1c6a6189fbd3d55e91df4ee1c62dd2ea300d6ebb
onirutlA <alturino001@gmail.com>
2022-04-03 11:01:54 +0700

    refactor: delete movie response and tv response to ItemResponse.kt

--- app/src/main/java/com/onirutla/movflex/data/source/remote/response/ItemResponse.kt
+++ app/src/main/java/com/onirutla/movflex/data/source/remote/response/ItemResponse.kt
@@ -1,10 +1,10 @@
-package com.onirutla.movflex.data.source.remote.response.movie
+package com.onirutla.movflex.data.source.remote.response
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class MovieResponse(
+data class ItemResponse(
     @Json(name = "id")
     val id: Int = 0,
     @Json(name = "poster_path")
