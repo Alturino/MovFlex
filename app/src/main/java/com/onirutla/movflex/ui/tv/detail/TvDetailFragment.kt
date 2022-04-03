3cedde6919419e6a82804bd841220bfa035fdc39
onirutlA <alturino001@gmail.com>
2022-04-03 15:34:22 +0700

    add: tv detail fragment observe from viewmodel

--- app/src/main/java/com/onirutla/movflex/ui/tv/detail/TvDetailFragment.kt
+++ app/src/main/java/com/onirutla/movflex/ui/tv/detail/TvDetailFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import com.onirutla.movflex.databinding.FragmentTvDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class TvDetailFragment : Fragment() {
 
     private var _binding: FragmentTvDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val args: TvDetailFragmentArgs by navArgs()
+    private val viewModel: TvDetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getTvDetail(args.tvId)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +42,14 @@ class TvDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.tvDetail.collect {
+                    binding.tv = it
+                    binding.format = "%.2f"
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
