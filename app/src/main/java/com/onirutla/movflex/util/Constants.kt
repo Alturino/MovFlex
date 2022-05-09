ea91f0678e342a593c7c146fdf1d5b36f36c9eb8
onirutlA <alturino001@gmail.com>
2022-05-09 22:38:56 +0700

    add: more constant in Constants.kt

--- app/src/main/java/com/onirutla/movflex/util/Constants.kt
+++ app/src/main/java/com/onirutla/movflex/util/Constants.kt
@@ -10,10 +10,16 @@ object Constants {
     const val API_TOKEN = BuildConfig.API_TOKEN
     const val BASE_URL = BuildConfig.BASE_URL
     const val BASE_IMAGE_PATH = BuildConfig.BASE_IMAGE_PATH
+
+    const val TITLE_MOVIE_UPCOMING = "Upcoming"
+    const val TITLE_MOVIE_TOP_RATED = "Top Rated"
+    const val TITLE_MOVIE_NOW_PLAYING = "Now Playing"
+
     const val TITLE_POPULAR = "Popular"
-    const val TITLE_UPCOMING = "Upcoming"
-    const val TITLE_TOP_RATED = "Top Rated"
-    const val TITLE_NOW_PLAYING = "Now Playing"
+
+    const val TITLE_TV_AIRING_TODAY = "Airing Today"
+    const val TITLE_TV_TOP_RATED = "Top Rated"
+    const val TITLE_TV_ON_THE_AIR = "On The Air"
 
     object ItemComparator : DiffUtil.ItemCallback<ItemResponse>() {
         override fun areItemsTheSame(
