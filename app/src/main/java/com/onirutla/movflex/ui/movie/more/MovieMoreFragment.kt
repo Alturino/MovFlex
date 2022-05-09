7d766c7a25157384b01dbfe12c8496c21f0403ce
onirutlA <alturino001@gmail.com>
2022-05-09 21:22:27 +0700

    add: MovieMoreFragment.kt

---
+++ app/src/main/java/com/onirutla/movflex/ui/movie/more/MovieMoreFragment.kt
@@ -0,0 +1,82 @@
+package com.onirutla.movflex.ui.movie.more
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.onirutla.movflex.databinding.FragmentMovieMoreBinding
+import com.onirutla.movflex.ui.adapter.ItemPagingAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@InternalCoroutinesApi
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class MovieMoreFragment : Fragment() {
+
+    private var _binding: FragmentMovieMoreBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MovieMoreViewModel by viewModels()
+
+    private val args: MovieMoreFragmentArgs by navArgs()
+
+    private val movieMoreAdapter by lazy {
+        ItemPagingAdapter { view, itemId ->
+            view.findNavController()
+                .navigate(
+                    MovieMoreFragmentDirections.actionMovieMoreFragmentToMovieDetailFragment(itemId)
+                )
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val category = args.category
+        viewModel.getMovieByCategory(category)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieMoreBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.moviePaging.apply {
+            adapter = movieMoreAdapter
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movie.collect {
+                    movieMoreAdapter.submitData(it)
+                }
+            }
+        }
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
