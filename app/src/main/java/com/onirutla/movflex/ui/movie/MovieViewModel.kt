26654a11b41e602e835d7cc69c584896d67e7373
onirutlA <alturino001@gmail.com>
2022-05-09 10:39:48 +0700

    refactor: change to movieUseCase

--- app/src/main/java/com/onirutla/movflex/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/onirutla/movflex/ui/movie/MovieViewModel.kt
@@ -2,43 +2,21 @@ package com.onirutla.movflex.ui.movie
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.onirutla.movflex.data.repository.movie.MovieRepository
-import com.onirutla.movflex.ui.Category
+import com.onirutla.movflex.usecase.MovieUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieViewModel @Inject constructor(
-    private val movieRepository: MovieRepository,
+    private val movieUseCase: MovieUseCase
 ) : ViewModel() {
 
-    private val popularMovie = movieRepository.getMoviePopularHome()
-    private val upcomingMovie = movieRepository.getMovieUpcomingHome()
-    private val topRatedMovie = movieRepository.getMovieTopRatedHome()
-    private val nowPlayingMovie = movieRepository.getMovieNowPlayingHome()
-
-    val movie = combine(
-        popularMovie,
-        upcomingMovie,
-        topRatedMovie,
-        nowPlayingMovie
-    ) { popular, upcoming, topRated, nowPlaying ->
-
-        val popularCategory = Category("Popular", popular)
-        val upcomingCategory = Category("Upcoming", upcoming)
-        val topRatedCategory = Category("Top Rated", topRated)
-        val nowPlayingCategory = Category("Now Playing", nowPlaying)
-
-        listOf(
-            popularCategory,
-            upcomingCategory,
-            topRatedCategory,
-            nowPlayingCategory
-        )
-
-    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+    val movie = movieUseCase.combinedCategory.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        emptyList()
+    )
 
 }
