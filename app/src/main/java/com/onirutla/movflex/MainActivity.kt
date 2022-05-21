de7f15dd1b77f6e0101750d022ecbe514509a95c
onirutlA <alturino001@gmail.com>
2022-05-21 14:03:45 +0700

    change design

--- app/src/main/java/com/onirutla/movflex/MainActivity.kt
+++ app/src/main/java/com/onirutla/movflex/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.onirutla.movflex
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.onirutla.movflex.databinding.ActivityMainBinding
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private val currentFragment: Fragment?
+        get() = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+            ?.childFragmentManager
+            ?.fragments
+            ?.first()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -21,5 +29,26 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         binding.bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.movieDetailFragment -> hideBottomAppbar()
+                R.id.tvDetailFragment -> hideBottomAppbar()
+                else -> showBottomAppbar()
+            }
+        }
+
+    }
+
+    private fun showBottomAppbar() {
+        binding.run {
+            bottomNav.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideBottomAppbar() {
+        binding.run {
+            bottomNav.visibility = View.GONE
+        }
     }
 }
