6a45325d5f8cd838d36d1f0cfb0e757064ef9f65
onirutlA <alturino001@gmail.com>
2022-06-21 22:06:55 +0700

    refactor: add explicit return type and change from using string to MovieType

--- app/src/main/java/com/onirutla/movflex/usecase/movie/MovieMoreUseCase.kt
+++ app/src/main/java/com/onirutla/movflex/usecase/movie/MovieMoreUseCase.kt
@@ -1,24 +1,22 @@
 package com.onirutla.movflex.usecase.movie
 
+import androidx.lifecycle.LiveData
+import androidx.paging.PagingData
 import com.onirutla.movflex.data.repository.movie.MovieRepository
-import com.onirutla.movflex.util.Constants.TITLE_MOVIE_NOW_PLAYING
-import com.onirutla.movflex.util.Constants.TITLE_MOVIE_TOP_RATED
-import com.onirutla.movflex.util.Constants.TITLE_MOVIE_UPCOMING
-import com.onirutla.movflex.util.Constants.TITLE_POPULAR
-import kotlinx.coroutines.flow.flow
+import com.onirutla.movflex.data.source.remote.response.ItemResponse
+import com.onirutla.movflex.util.MovieType
 import javax.inject.Inject
 
 class MovieMoreUseCase @Inject constructor(
     private val movieRepository: MovieRepository
 ) {
 
-    operator fun invoke(category: String) = when (category) {
-        TITLE_MOVIE_TOP_RATED -> movieRepository.getMovieTopRatedPaging()
-        TITLE_MOVIE_NOW_PLAYING -> movieRepository.getMovieNowPlayingPaging()
-        TITLE_POPULAR -> movieRepository.getMoviePopularPaging()
-        TITLE_MOVIE_UPCOMING -> movieRepository.getMovieUpcomingPaging()
-        else -> flow { }
-    }
-
+    operator fun invoke(movieType: MovieType): LiveData<PagingData<ItemResponse>> =
+        when (movieType) {
+            MovieType.MOVIE_UPCOMING -> movieRepository.getMovieUpcomingPaging()
+            MovieType.MOVIE_TOP_RATED -> movieRepository.getMovieTopRatedPaging()
+            MovieType.MOVIE_NOW_PLAYING -> movieRepository.getMovieNowPlayingPaging()
+            MovieType.MOVIE_POPULAR -> movieRepository.getMoviePopularPaging()
+        }
 
 }
