d690206b1115ddbd11f81fa1556e9dc2886f2ee6
onirutlA <alturino001@gmail.com>
2022-06-22 07:44:28 +0700

    refactor: TvMoreUseCase.kt param from string to TvType

--- app/src/main/java/com/onirutla/movflex/usecase/tv/TvMoreUseCase.kt
+++ app/src/main/java/com/onirutla/movflex/usecase/tv/TvMoreUseCase.kt
@@ -1,22 +1,17 @@
 package com.onirutla.movflex.usecase.tv
 
 import com.onirutla.movflex.data.repository.tv.TvRepository
-import com.onirutla.movflex.util.Constants.TITLE_POPULAR
-import com.onirutla.movflex.util.Constants.TITLE_TV_AIRING_TODAY
-import com.onirutla.movflex.util.Constants.TITLE_TV_ON_THE_AIR
-import com.onirutla.movflex.util.Constants.TITLE_TV_TOP_RATED
-import kotlinx.coroutines.flow.flow
+import com.onirutla.movflex.util.TvType
 import javax.inject.Inject
 
 class TvMoreUseCase @Inject constructor(
     private val tvRepository: TvRepository
 ) {
 
-    operator fun invoke(category: String) = when (category) {
-        TITLE_TV_TOP_RATED -> tvRepository.getTvTopRatedPaging()
-        TITLE_TV_ON_THE_AIR -> tvRepository.getTvOnTheAirPaging()
-        TITLE_POPULAR -> tvRepository.getTvPopularPaging()
-        TITLE_TV_AIRING_TODAY -> tvRepository.getTvAiringTodayPaging()
-        else -> flow { }
+    operator fun invoke(tvType: TvType) = when (tvType) {
+        TvType.TV_TOP_RATED -> tvRepository.getTvTopRatedPaging()
+        TvType.TV_ON_THE_AIR -> tvRepository.getTvOnTheAirPaging()
+        TvType.TV_POPULAR -> tvRepository.getTvPopularPaging()
+        TvType.TV_AIRING_TODAY -> tvRepository.getTvAiringTodayPaging()
     }
 }
