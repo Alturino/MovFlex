817ae673b4301764c56d4301dd344e3302174099
onirutlA <alturino001@gmail.com>
2022-06-25 13:50:27 +0700

    refactor: change dependency from movieApiService to movieRemoteDataSource

--- app/src/main/java/com/onirutla/movflex/data/repository/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/onirutla/movflex/data/repository/movie/MovieRepositoryImpl.kt
@@ -6,117 +6,100 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.liveData
+import com.onirutla.movflex.data.source.PagingDataSource
 import com.onirutla.movflex.data.source.local.dao.FavoriteDao
 import com.onirutla.movflex.data.source.local.entities.FavoriteEntity
-import com.onirutla.movflex.data.source.remote.PagingDataSource
-import com.onirutla.movflex.data.source.remote.response.ItemResponse
+import com.onirutla.movflex.data.source.remote.movie.MovieRemoteDataSource
+import com.onirutla.movflex.data.source.remote.response.ItemDto
 import com.onirutla.movflex.data.source.remote.response.movie.toEntity
-import com.onirutla.movflex.data.source.remote.service.MovieApiService
 import com.onirutla.movflex.util.Constants.PAGE_SIZE
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @ViewModelScoped
 class MovieRepositoryImpl @Inject constructor(
-    private val movieApiService: MovieApiService,
+    private val remoteDataSource: MovieRemoteDataSource,
     private val favoriteDao: FavoriteDao
 ) : MovieRepository {
 
-    override fun getMoviePopularPaging(): LiveData<PagingData<ItemResponse>> = Pager(
+    companion object {
+        val TAG: String = this::class.java.simpleName
+    }
+
+    override fun getMoviePopularPaging(): LiveData<PagingData<ItemDto>> = Pager(
         config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
         pagingSourceFactory = {
-            PagingDataSource { position -> movieApiService.getMoviePopular(position) }
+            PagingDataSource { position -> remoteDataSource.getMoviePopular(position) }
         },
     ).liveData
 
-    override fun getMoviePopularHome(): Flow<List<ItemResponse>> = flow {
-        val response = movieApiService.getMoviePopular()
-        if (response.isSuccessful)
-            emit(response.body()!!.results)
-        else
-            emit(emptyList())
+    override fun getMoviePopularHome(): Flow<List<ItemDto>> = flow {
+        val dto = remoteDataSource.getMoviePopular()
+        emit(dto)
     }.catch {
-        Log.d(this@MovieRepositoryImpl.javaClass.simpleName, "$it")
+        Log.d(TAG, "$it")
         emit(emptyList())
     }
 
-    override fun getMovieNowPlayingPaging(): LiveData<PagingData<ItemResponse>> = Pager(
+    override fun getMovieNowPlayingPaging(): LiveData<PagingData<ItemDto>> = Pager(
         config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
         pagingSourceFactory = {
-            PagingDataSource { position -> movieApiService.getMovieNowPlaying(position) }
+            PagingDataSource { position -> remoteDataSource.getMovieNowPlaying(position) }
         },
     ).liveData
 
-    override fun getMovieNowPlayingHome(): Flow<List<ItemResponse>> = flow {
-        val response = movieApiService.getMovieNowPlaying()
-        if (response.isSuccessful)
-            emit(response.body()!!.results)
-        else
-            emit(emptyList())
+    override fun getMovieNowPlayingHome(): Flow<List<ItemDto>> = flow {
+        val dto = remoteDataSource.getMovieNowPlaying()
+        emit(dto)
     }.catch {
-        Log.d(this@MovieRepositoryImpl.javaClass.simpleName, "$it")
+        Log.d(TAG, "$it")
         emit(emptyList())
     }
 
-    override fun getMovieTopRatedPaging(): LiveData<PagingData<ItemResponse>> = Pager(
+    override fun getMovieTopRatedPaging(): LiveData<PagingData<ItemDto>> = Pager(
         config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
         pagingSourceFactory = {
-            PagingDataSource { position -> movieApiService.getMovieTopRated(position) }
+            PagingDataSource { position -> remoteDataSource.getMovieTopRated(position) }
         }
     ).liveData
 
-    override fun getMovieTopRatedHome(): Flow<List<ItemResponse>> = flow {
-        val response = movieApiService.getMovieTopRated()
-        if (response.isSuccessful)
-            emit(response.body()!!.results)
-        else
-            emit(emptyList())
+    override fun getMovieTopRatedHome(): Flow<List<ItemDto>> = flow {
+        val dto = remoteDataSource.getMovieTopRated()
+        emit(dto)
     }.catch {
-        Log.d(this@MovieRepositoryImpl.javaClass.simpleName, "$it")
+        Log.d(TAG, "$it")
         emit(emptyList())
     }
 
 
-    override fun getMovieUpcomingPaging(): LiveData<PagingData<ItemResponse>> = Pager(
+    override fun getMovieUpcomingPaging(): LiveData<PagingData<ItemDto>> = Pager(
         config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
         pagingSourceFactory = {
-            PagingDataSource { position -> movieApiService.getMovieUpcoming(position) }
+            PagingDataSource { position -> remoteDataSource.getMovieUpcoming(position) }
         }
     ).liveData
 
-    override fun getMovieUpcomingHome(): Flow<List<ItemResponse>> = flow {
-        val response = movieApiService.getMovieUpcoming()
-        if (response.isSuccessful)
-            emit(response.body()!!.results)
-        else
-            emit(emptyList())
+    override fun getMovieUpcomingHome(): Flow<List<ItemDto>> = flow {
+        val dto = remoteDataSource.getMovieUpcoming()
+        emit(dto)
     }.catch {
-        Log.d(this@MovieRepositoryImpl.javaClass.simpleName, "$it")
+        Log.d(TAG, "$it")
         emit(emptyList())
     }
 
-
-    override fun getMovieDetail(id: Int): Flow<FavoriteEntity> =
-        favoriteDao.isFavorite(id).map {
-            if (it != null) {
-                it
-            } else {
-                val response = movieApiService.getMovieDetail(id)
-                if (response.isSuccessful) {
-                    favoriteDao.insertFavorite(response.body()!!.toEntity())
-                    response.body()!!.toEntity()
-                } else {
-                    FavoriteEntity()
-                }
-            }
-        }.catch {
-            Log.d(this@MovieRepositoryImpl.javaClass.simpleName, "$it")
-            emit(FavoriteEntity())
+    override fun getMovieDetail(id: Int): Flow<FavoriteEntity> = favoriteDao.isFavorite(id).map {
+        if (it != null)
+            it
+        else {
+            val response = remoteDataSource.getMovieDetail(id)
+            response.toEntity()
         }
+    }.catch {
+        Log.d(TAG, "$it")
+        emit(FavoriteEntity())
+    }
 }
