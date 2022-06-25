c97b472f0c5dd8ee92973d835a187482d066d425
onirutlA <alturino001@gmail.com>
2022-06-25 13:53:23 +0700

    refactor: ItemResponse.kt to ItemDto

--- app/src/main/java/com/onirutla/movflex/data/repository/tv/TvRepository.kt
+++ app/src/main/java/com/onirutla/movflex/data/repository/tv/TvRepository.kt
@@ -3,22 +3,22 @@ package com.onirutla.movflex.data.repository.tv
 import androidx.lifecycle.LiveData
 import androidx.paging.PagingData
 import com.onirutla.movflex.data.source.local.entities.FavoriteEntity
-import com.onirutla.movflex.data.source.remote.response.ItemResponse
+import com.onirutla.movflex.data.source.remote.response.ItemDto
 import kotlinx.coroutines.flow.Flow
 
 interface TvRepository {
 
-    fun getTvPopularPaging(): LiveData<PagingData<ItemResponse>>
-    fun getTvPopularHome(): Flow<List<ItemResponse>>
+    fun getTvPopularPaging(): LiveData<PagingData<ItemDto>>
+    fun getTvPopularHome(): Flow<List<ItemDto>>
 
-    fun getTvOnTheAirPaging(): LiveData<PagingData<ItemResponse>>
-    fun getTvOnTheAirHome(): Flow<List<ItemResponse>>
+    fun getTvOnTheAirPaging(): LiveData<PagingData<ItemDto>>
+    fun getTvOnTheAirHome(): Flow<List<ItemDto>>
 
-    fun getTvTopRatedPaging(): LiveData<PagingData<ItemResponse>>
-    fun getTvTopRatedHome(): Flow<List<ItemResponse>>
+    fun getTvTopRatedPaging(): LiveData<PagingData<ItemDto>>
+    fun getTvTopRatedHome(): Flow<List<ItemDto>>
 
-    fun getTvAiringTodayPaging(): LiveData<PagingData<ItemResponse>>
-    fun getTvAiringTodayHome(): Flow<List<ItemResponse>>
+    fun getTvAiringTodayPaging(): LiveData<PagingData<ItemDto>>
+    fun getTvAiringTodayHome(): Flow<List<ItemDto>>
 
     fun getTvDetail(id: Int): Flow<FavoriteEntity>
 }
