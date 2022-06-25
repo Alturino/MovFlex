1a5f7183a84d1b67c10cf21fd405643b958dcf35
onirutlA <alturino001@gmail.com>
2022-06-25 13:53:55 +0700

    add: mapper method

--- app/src/main/java/com/onirutla/movflex/data/source/remote/response/ItemResponse.kt
+++ app/src/main/java/com/onirutla/movflex/data/source/remote/response/ItemResponse.kt
@@ -16,3 +16,12 @@ data class ItemResponse(
     @Json(name = "vote_average")
     val voteAverage: Float? = 0f,
 )
+
+fun ItemResponse.toDto(): ItemDto = ItemDto(
+    id = id,
+    backdropPath = posterPath.orEmpty(),
+    title = if (name.isNullOrEmpty()) "$title" else name,
+    posterPath = posterPath.orEmpty(),
+    voteAverage = voteAverage ?: 0f,
+    isFavorite = false,
+)
