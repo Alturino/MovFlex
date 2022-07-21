ce5513c66611e104d2cfbaadc08bde3a495bc6af
onirutlA <alturino001@gmail.com>
2022-07-21 11:37:27 +0700

    refactor: delete unneeded favorite comparator in Constants.kt

--- core/src/main/java/com/onirutla/movflex/util/Constants.kt
+++ core/src/main/java/com/onirutla/movflex/util/Constants.kt
@@ -2,7 +2,6 @@ package com.onirutla.movflex.util
 
 import androidx.recyclerview.widget.DiffUtil
 import com.onirutla.movflex.core.BuildConfig
-import com.onirutla.movflex.data.source.local.entities.FavoriteEntity
 import com.onirutla.movflex.domain.model.Content
 
 object Constants {
@@ -11,18 +10,6 @@ object Constants {
     const val API_TOKEN = BuildConfig.API_TOKEN
     const val BASE_URL = BuildConfig.BASE_URL
     const val BASE_IMAGE_PATH = BuildConfig.BASE_IMAGE_PATH
-
-    object FavoriteComparator : DiffUtil.ItemCallback<FavoriteEntity>() {
-        override fun areItemsTheSame(
-            oldItem: FavoriteEntity,
-            newItem: FavoriteEntity
-        ): Boolean = oldItem.id == newItem.id
-
-        override fun areContentsTheSame(
-            oldItem: FavoriteEntity,
-            newItem: FavoriteEntity
-        ): Boolean = oldItem == newItem
-    }
 
     object ItemComparator : DiffUtil.ItemCallback<Content>() {
         override fun areItemsTheSame(
