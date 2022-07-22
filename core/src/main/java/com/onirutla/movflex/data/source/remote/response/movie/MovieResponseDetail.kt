5c685925f2e262a821f4c5310a4fb215580fdfb5
onirutlA <alturino001@gmail.com>
2022-07-22 11:26:50 +0700

    add generate adapter to movie response detail

--- core/src/main/java/com/onirutla/movflex/data/source/remote/response/movie/MovieResponseDetail.kt
+++ core/src/main/java/com/onirutla/movflex/data/source/remote/response/movie/MovieResponseDetail.kt
@@ -1,7 +1,9 @@
 package com.onirutla.movflex.data.source.remote.response.movie
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class MovieResponseDetail(
     @Json(name = "id")
     val id: Int? = 0,
