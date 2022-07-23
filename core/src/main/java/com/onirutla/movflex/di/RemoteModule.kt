36966ce62822ed5c2f57f64b0baaea691b42518e
onirutlA <alturino001@gmail.com>
2022-07-23 12:31:35 +0700

    add certificate pinner to okhttp client

--- core/src/main/java/com/onirutla/movflex/di/RemoteModule.kt
+++ core/src/main/java/com/onirutla/movflex/di/RemoteModule.kt
@@ -1,6 +1,10 @@
 package com.onirutla.movflex.di
 
 import com.onirutla.movflex.core.BuildConfig
+import com.onirutla.movflex.core.BuildConfig.CERTIFICATE_0
+import com.onirutla.movflex.core.BuildConfig.CERTIFICATE_1
+import com.onirutla.movflex.core.BuildConfig.CERTIFICATE_2
+import com.onirutla.movflex.core.BuildConfig.HOST_NAME
 import com.onirutla.movflex.data.source.remote.service.MovieApiService
 import com.onirutla.movflex.data.source.remote.service.TvApiService
 import com.onirutla.movflex.util.Constants.API_TOKEN
@@ -11,11 +15,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -24,51 +30,50 @@ object RemoteModule {
 
     @Singleton
     @Provides
-    fun provideMoshiBuilder(): Moshi = Moshi
-        .Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
+    fun provideHttpClient(): OkHttpClient {
 
-    @Singleton
-    @Provides
-    fun provideInterceptor(): Interceptor = Interceptor {
-        val request = it.request()
-            .newBuilder()
-            .addHeader("Authorization", "Bearer $API_TOKEN")
+        val interceptor = Interceptor {
+            val request = it.request()
+                .newBuilder()
+                .addHeader("Authorization", "Bearer $API_TOKEN")
+                .build()
+            it.proceed(request)
+        }
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add(HOST_NAME, CERTIFICATE_0)
+            .add(HOST_NAME, CERTIFICATE_1)
+            .add(HOST_NAME, CERTIFICATE_2)
             .build()
-        it.proceed(request)
-    }
 
-    @Singleton
-    @Provides
-    fun provideLogger(): HttpLoggingInterceptor = HttpLoggingInterceptor()
-        .apply {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
             if (BuildConfig.DEBUG)
                 level = HttpLoggingInterceptor.Level.BODY
         }
 
-    @Singleton
-    @Provides
-    fun provideHttpClient(
-        interceptor: Interceptor,
-        loggingInterceptor: HttpLoggingInterceptor
-    ): OkHttpClient = OkHttpClient
-            .Builder()
+        return OkHttpClient.Builder()
             .addInterceptor(interceptor)
             .addInterceptor(loggingInterceptor)
+            .certificatePinner(certificatePinner)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
             .build()
+    }
 
     @Singleton
     @Provides
-    fun provideRetrofitInstance(
-        moshi: Moshi,
-        okHttpClient: OkHttpClient
-    ): Retrofit = Retrofit
-            .Builder()
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .baseUrl(BASE_URL)
             .client(okHttpClient)
             .build()
+    }
 
     @Singleton
     @Provides
