d111d8acb1d64fb4aee9d06d0263abec40fdc321
onirutlA <alturino001@gmail.com>
2023-03-25 14:24:27 +0700

    add: add build.gradle.kts to movie module

---
+++ movie/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id(Plugin.safeArg)
+    id(Plugin.daggerHilt)
+    id(Plugin.mapsSecret)
+}
+
+android {
+    namespace = "com.onirutla.movflex.movie"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
+        consumerProguardFiles.add(File("consumer-rules.pro"))
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = AppConfig.isReleaseMinifyEnabled
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = AppConfig.sourceCompatibility
+        targetCompatibility = AppConfig.targetCompatibility
+    }
+    kotlinOptions {
+        jvmTarget = AppConfig.kotlinJvmTarget
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+    applyShared()
+    applyRetrofit()
+    applyOkhttp()
+    applyMoshi()
+    implementation(project(Modules.core))
+}
