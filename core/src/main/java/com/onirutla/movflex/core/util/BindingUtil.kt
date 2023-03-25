03d18c09facca46169c8a23470f86c8e76b3ac4c
onirutlA <alturino001@gmail.com>
2023-03-25 14:19:33 +0700

    change: BindingUtil.kt to multi line

--- core/src/main/java/com/onirutla/movflex/core/util/BindingUtil.kt
+++ core/src/main/java/com/onirutla/movflex/core/util/BindingUtil.kt
@@ -7,5 +7,10 @@ import com.onirutla.movflex.core.util.Constants.BASE_IMAGE_PATH
 
 @BindingAdapter("load_image")
 fun loadImage(imageView: ImageView, any: Any?) {
-    any?.let { Glide.with(imageView.context).load("$BASE_IMAGE_PATH$any").into(imageView).clearOnDetach() }
+    if (any != null) {
+        Glide.with(imageView.context)
+            .load("$BASE_IMAGE_PATH$any")
+            .into(imageView)
+            .clearOnDetach()
+    }
 }
