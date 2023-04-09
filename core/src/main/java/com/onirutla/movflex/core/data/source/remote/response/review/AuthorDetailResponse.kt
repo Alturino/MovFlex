fe26facbd31f8c2203b53d85786325e92a6ca4a0
onirutlA <alturino001@gmail.com>
2023-04-09 15:20:14 +0700

    add AuthorDetail.kt, AuthorDetailResponse.kt

---
+++ core/src/main/java/com/onirutla/movflex/core/data/source/remote/response/review/AuthorDetailResponse.kt
@@ -0,0 +1,16 @@
+package com.onirutla.movflex.core.data.source.remote.response.review
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AuthorDetailResponse(
+    @Json(name = "avatar_path")
+    val avatarPath: String,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "rating")
+    val rating: Int,
+    @Json(name = "username")
+    val username: String
+)
---
+++ core/src/main/java/com/onirutla/movflex/core/domain/model/review/AuthorDetail.kt
@@ -0,0 +1,8 @@
+package com.onirutla.movflex.core.domain.model.review
+
+data class AuthorDetail(
+    val avatarPath: String,
+    val name: String,
+    val rating: Int,
+    val username: String,
+)
