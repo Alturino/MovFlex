dd31b21ebef7bafdb36077bfcca52449b401f8c0
onirutlA <alturino001@gmail.com>
2023-04-09 15:22:17 +0700

    move ItemComparator from Constants.kt to ContentComparator.kt

--- core/src/main/java/com/onirutla/movflex/core/util/Constants.kt
+++ core/src/main/java/com/onirutla/movflex/core/util/Constants.kt
@@ -1,8 +1,6 @@
 package com.onirutla.movflex.core.util
 
-import androidx.recyclerview.widget.DiffUtil
 import com.onirutla.movflex.core.BuildConfig
-import com.onirutla.movflex.core.domain.model.Content
 
 object Constants {
     const val TMDB_STARTING_PAGE_INDEX = 1
@@ -11,16 +9,4 @@ object Constants {
     const val BASE_URL = BuildConfig.BASE_URL
     const val BASE_IMAGE_PATH = BuildConfig.BASE_IMAGE_PATH
     const val DB_PASSPHRASE = BuildConfig.DB_PASSPRHASE
-
-    object ItemComparator : DiffUtil.ItemCallback<Content>() {
-        override fun areItemsTheSame(
-            oldItem: Content,
-            newItem: Content,
-        ): Boolean = oldItem.id == newItem.id
-
-        override fun areContentsTheSame(
-            oldItem: Content,
-            newItem: Content,
-        ): Boolean = oldItem.title == newItem.title && oldItem.description == newItem.description
-    }
 }
